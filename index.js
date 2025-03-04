5cafcd32a458789dd48f8edf9cb1d9ef15c12117
hectorvsqz <155773340+hectorvsqz@users.noreply.github.com>
2025-03-03 20:58:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World RA24.3');
 });
 
 let server; // Declare server outside listen()
