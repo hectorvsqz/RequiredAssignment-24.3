a750f159ac2b8c10afe703e15370ccd423a5fcdf
hectorvsqz <155773340+hectorvsqz@users.noreply.github.com>
2025-03-03 20:55:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // index.js
 const express = require('express');
 const app = express();
-
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log(`Server listening at http://localhost:3000`);
-});
+let server; // Declare server outside listen()
+
+const startServer = () => {
+  server = app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+  return server;
+};
 
 const closeServer = () => {
   if (server) {
