239c71b2c09c9b3bd0d44a13f7ca134f3b49c65f
hectorvsqz <155773340+hectorvsqz@users.noreply.github.com>
2025-03-03 20:53:03 -0500

    Update index.js
    added code to close connection

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ app.listen(3000, () => {
   console.log(`Server listening at http://localhost:3000`);
 });
 
+const closeServer = () => {
+  if (server) {
+    server.close();
+  }
+};
+
+if (require.main === module) {
+  startServer(); // Only start the server if this file is run directly
+}
+
 module.exports = app;
+module.exports.startServer = startServer;
+module.exports.closeServer = closeServer;
